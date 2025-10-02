27f89e3ecdfdd854dbcf6d6fed7861c6d71b6c4a
JockaliaS <144474737+JockaliaS@users.noreply.github.com>
2025-10-02 11:51:45 -0400

    Added .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,43 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-import Navigation from '@/components/Navigation';
-import HomePage from '@/pages/HomePage';
-import QuizPage from '@/pages/QuizPage';
-import DirectoryPage from '@/pages/DirectoryPage';
-import CreationsDirectoryPage from '@/pages/CreationsDirectoryPage';
-import ElinePage from '@/pages/ElinePage';
-import TherapistProfile from '@/pages/TherapistProfile';
-import RegisterTherapistPage from '@/pages/RegisterTherapistPage';
-import BlogPage from '@/pages/BlogPage';
-import FormesEtFrequencesPage from '@/pages/blog/FormesEtFrequencesPage';
-import ResonanceSchumannPage from '@/pages/blog/ResonanceSchumannPage';
-import EauMemoirePage from '@/pages/blog/EauMemoirePage';
-import DirectoryGatePage from '@/pages/DirectoryGatePage';
-import RegistrationTypePage from '@/pages/RegistrationTypePage';
-import EditTherapistProfilePage from '@/pages/EditTherapistProfilePage';
-import MyInnerJourneyPage from '@/pages/MyInnerJourneyPage';
-import ServiceDetailPage from '@/pages/ServiceDetailPage';
-import RituelDomeEauTerrePage from '@/pages/RituelDomeEauTerrePage';
-import JourneyResultsPage from '@/pages/JourneyResultsPage';
-import TerrePage from '@/pages/TerrePage';
-import EauPage from '@/pages/EauPage';
-import FeuPage from '@/pages/FeuPage';
-import AirPage from '@/pages/AirPage';
-import EtherPage from '@/pages/EtherPage';
-import TherapistGatePage from '@/pages/TherapistGatePage';
-import ContactPage from '@/pages/ContactPage';
-import { Toaster } from '@/components/ui/toaster';
-import ScrollToTop from '@/components/ScrollToTop';
-import AdminDashboard from '@/components/admin/AdminDashboard';
-import ProtectedRoute from '@/components/auth/ProtectedRoute';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CharterPage from './pages/CharterPage';
+import DirectoryPage from './pages/DirectoryPage';
+import ArtistsDirectoryPage from './pages/ArtistsDirectoryPage';
+import ArtistProfile from './pages/ArtistProfile';
+import TherapistProfile from './pages/TherapistProfile';
+import RegisterTherapistPage from './pages/RegisterTherapistPage';
+import RegisterArtistPage from './pages/RegisterArtistPage';
+import QuizPage from './pages/QuizPage';
+import QuizResultPage from './pages/QuizResultPage';
+import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
+import JourneyPage from './pages/JourneyPage';
+import AdminPage from './pages/AdminPage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
-    <div className="min-h-screen sacred-geometry">
-      <Helmet>
-        <title>Terra Nova — Mon Voyage Intérieur</title>
-        <meta name="description" content="Terra Nova : Plateforme holistique interactive pour composer votre journée bien-être sur mesure en Guadeloupe. Connectez-vous aux thérapeutes, artistes et lieux sacrés." />
-      </Helmet>
-      
-      <ScrollToTop />
-      <Navigation />
-      
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/charter" element={<CharterPage />} />
+        <Route path="/directory" element={<DirectoryPage />} />
+        <Route path="/artists-directory" element={<ArtistsDirectoryPage />} />
+        <Route path="/therapist/:id" element={<TherapistProfile />} />
+        <Route path="/artist/:id" element={<ArtistProfile />} />
+        <Route path="/register-therapist" element={<RegisterTherapistPage />} />
+        <Route path="/register-artist" element={<RegisterArtistPage />} />
         <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/annuaire" element={<DirectoryPage />} />
-        <Route path="/annuaire-creations" element={<CreationsDirectoryPage />} />
-        <Route path="/annuaire-gate" element={<DirectoryGatePage />} />
-        <Route path="/eline-dracon" element={<ElinePage />} />
-        <Route path="/soin/rituel-dome-eau-terre" element={<RituelDomeEauTerrePage />} />
-        <Route path="/soin/:serviceId" element={<ServiceDetailPage />} />
-        <Route path="/therapeute/:id" element={<TherapistProfile />} />
-        <Route path="/rejoindre-aventure" element={<TherapistGatePage />} />
-        <Route path="/inscription-type" element={<RegistrationTypePage />} />
-        <Route path="/inscription-formulaire" element={<RegisterTherapistPage />} />
+        <Route path="/quiz-result" element={<QuizResultPage />} />
         <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/formes-et-frequences" element={<FormesEtFrequencesPage />} />
-        <Route path="/blog/resonance-schumann" element={<ResonanceSchumannPage />} />
-        <Route path="/blog/eau-memoire" element={<EauMemoirePage />} />
-        <Route path="/mon-compte/modifier-profil" element={<EditTherapistProfilePage />} />
-        <Route path="/mon-voyage-interieur" element={<MyInnerJourneyPage />} />
-        <Route path="/mon-voyage-interieur/resultats" element={<JourneyResultsPage />} />
-        <Route path="/porte/terre" element={<TerrePage />} />
-        <Route path="/porte/eau" element={<EauPage />} />
-        <Route path="/porte/feu" element={<FeuPage />} />
-        <Route path="/porte/air" element={<AirPage />} />
-        <Route path="/porte/ether" element={<EtherPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/admin" element={
-          <ProtectedRoute requiredRole="admin">
-            <AdminDashboard />
-          </ProtectedRoute>
-        } />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="/journey" element={<JourneyPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
-      <Toaster />
-    </div>
+    </Router>
   );
 }
 
